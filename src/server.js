6618e4b30b4d3db4b8c1a13d860e6550f02cdeb8
Kyle Marden <demolaze@Mac.home>
2026-06-06 15:36:02 +0100

    Add routes, env config and request logging

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
+require("dotenv").config();
+
+// const morgan = require("morgan");
+
 const express = require("express");
+const healthRouter = require("./routes/health");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Refresh app running</h1>");
+app.use("/api", healthRouter);
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+
+  next();
 });
+
+// app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Refresh app running</h1>");
